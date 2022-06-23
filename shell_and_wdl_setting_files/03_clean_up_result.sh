25b39df28c9438b75a1c7186720a618ba379477a
Science Peak <45578740+sciencepeak@users.noreply.github.com>
2022-06-22 22:06:32 -0700

    Create 03_clean_up_result.sh

---
+++ shell_and_wdl_setting_files/03_clean_up_result.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# mprof run --include-children bash 03_clean_up_result.sh
+# mprof plot
+
+# Delete the runtime files in the very end, if not deleted previously.
+password_file_path=/media/hugolab/DataRAID1/projects/workflows/password.txt
+current_wdl_runtime_directory="$PWD"
+echo $(cat $password_file_path) | sudo -S rm -r ${current_wdl_runtime_directory}/cromwell-executions ${current_wdl_runtime_directory}/cromwell-workflow-logs
+
+echo "The script runs up to here!"
+
+
